b26b997c011d1edbc8fd53280b3163a88478995d
Yesid Sanabria <yesanabria2@poligran.edu.co>
2025-09-16 15:22:20 -0500

    Links V1.1

--- src/scripts/link-scroll-active.js
+++ src/scripts/link-scroll-active.js
@@ -1,25 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll('.nav-link');
-    const sections = document.querySelectorAll('section');
-    
-    const highlightNav = () => {
-      let currentSection = "";
-      
-      sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-          currentSection = section.id;
-        }
-      });
-      
-      links.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href') === `#${currentSection}`) {
-          link.classList.add('active');
-        }
-      });
-    };
+  const links = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('section');
+  
+  const highlightNav = () => {
+    let currentSection = "";
 
-    window.addEventListener('scroll', highlightNav);
-    highlightNav();
-  });
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+        currentSection = section.id;
+      }
+    });
+
+    if (window.scrollY === 0) {
+      currentSection = "inicio";
+    }
+
+    links.forEach(link => {
+      link.classList.remove('active');
+      const sectionId = link.getAttribute('href').split('#')[1];
+      if (sectionId && sectionId === currentSection) {
+        link.classList.add('active');
+      }
+    });
+  };
+
+  window.addEventListener('scroll', highlightNav);
+  highlightNav();
+});
